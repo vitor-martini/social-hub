2d1fbfeb74f71f316730614dc158e059d44b14c3
Vitor Martini <vitor-martini@outlook.com>
2024-02-03 16:13:05 -0300

    add: dark-light mode switch

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/sunglasses.png")
+  } else {
+    img.setAttribute("src", "https://github.com/vitor-martini.png")
+  }
+}
